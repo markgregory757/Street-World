87a7d8c2f80b761613fb51ffdb3b18e1d8b90d52
Mark Gomez <mark.g.gomez@gmail.com>
2022-02-17 04:46:20 -0800

    added a globe to the page

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 import { countryInfo } from "./countryAPI.js";
 import { countryArt } from "./streetArt.js";
-import { generateFeatureCollection } from "./mapAPI.js";
-import { createFeaturesGraphics } from "./mapAPI.js";
-import { createFeatureLayerFromGraphic } from "./mapAPI.js";
-import { addShapefileToMap } from "./mapAPI.js";
 
 
 
 countryArt();
 countryInfo();
+
+
+import { generateFeatureCollection } from "./mapAPI.js";
+import { createFeaturesGraphics } from "./mapAPI.js";
+import { createFeatureLayerFromGraphic } from "./mapAPI.js";
+import { addShapefileToMap } from "./mapAPI.js";
 generateFeatureCollection();
 createFeaturesGraphics();
 createFeatureLayerFromGraphic();
