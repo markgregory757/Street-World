31751996108744743591077bda7260f58b8a14c2
Mark Gomez <mark.g.gomez@gmail.com>
2022-02-18 14:50:11 -0800

    updating to the latest version

--- mapAPI.js
+++ mapAPI.js
@@ -1,13 +1,7 @@
 // add fetch request to populate with pin on flag selection
 // export your function to run in script file
-<<<<<<< HEAD
-const mapDIV = document.getElementById("map");
-console.dir(mapDIV);
-fetch("https://google-maps-geocoding-plus.p.rapidapi.com/geocode?address=121%20Buffalo%20View%20Ln%2C%20Georgetown%2C%20TX%2078628&language=en")
-.then(response => {
-	console.log(response);
-    return response.json()
-=======
+
+
 
 
 fetch("https://travel-places.p.rapidapi.com/", {
@@ -19,7 +13,7 @@ fetch("https://travel-places.p.rapidapi.com/", {
 })
 .then(response => {
 	console.log(response);
->>>>>>> bd9ec194a07f9ab51a80f23981ac24adc9e55756
+ bd9ec194a07f9ab51a80f23981ac24adc9e55756
 })
 .catch(err => {
 	console.error(err);
