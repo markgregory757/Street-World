76fd72c59abe94aceca3e2e691cdf9d267b73e4a
Mark Gomez <mark.g.gomez@gmail.com>
2022-02-19 15:03:40 -0800

    updates

--- streetArt.js
+++ streetArt.js
@@ -1,23 +1,12 @@
 const countryArt = (country) => {
 
  const streetArtDIV = document.getElementById("streetArtDIV")
-<<<<<<< HEAD
-
-const mapDIV = document.getElementById('mapDIV')
-console.log(mapDIV);
-    fetch(`https://street-art.p.rapidapi.com/country/${country}`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "street-art.p.rapidapi.com",
-            "x-rapidapi-key": "256bdc8139mshc95b18bdc3d05e8p102f0bjsnf45913349eec"
-=======
  
  fetch(`https://street-art.p.rapidapi.com/country/${country}`, {
      "method": "GET",
      "headers": {
          "x-rapidapi-host": "street-art.p.rapidapi.com",
          "x-rapidapi-key": "256bdc8139mshc95b18bdc3d05e8p102f0bjsnf45913349eec"
->>>>>>> main
         }
     })
     .then(response => response.json())
@@ -27,15 +16,11 @@ console.log(mapDIV);
             if (index <= 2) {
 
                 streetArtDIV.innerHTML += `<img src=${streetArtInfo.pictLink} style="height: 400px; width: 450px;"/> `
-<<<<<<< HEAD
                 mapDIV.innerHTML = `
                 <img src="${streetArtInfo.linkOpenStreetMap}" >
                 `
                 console.log(streetArtInfo.linkOpenStreetMap, 11111111)
             console.log(streetArtInfo.pictLink)
-=======
-                console.log(streetArtInfo.pictLink)
->>>>>>> main
             }
         });
     })
